b0b3f4a6754aed49f07da28bce3c66c3b1d69333
koui <kouifillwer@gmail.com>
2018-02-13 18:25:33 +0700

    test

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <a href="productdetails.php">productdetails</a>
     <a href="client_product_detail.php">client product detail</a>
     <a href="client_store.php">client store</a>
-    <a href="admin.php">admin</a>
+    <a href="admin.php">admin </a>
     <a href="admin_promotion_ads.php"> admin promotion</a>
     <a href="admin_search_keyword.php">admin search</a>
   </div>
